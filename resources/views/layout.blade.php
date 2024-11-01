07dc4431084d102f4a3cb5dc295aacd7730e73f9
Tunn <wolf123cpr@gmail.com>
2024-11-01 07:48:33 +0700

    Tuan them file layout trong views

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Documentation 2</h2> {{--Thêm dòng này --}}
+</body>
+</html>
